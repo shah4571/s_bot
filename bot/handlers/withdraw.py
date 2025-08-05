74380743c139e87535c278896b8b1f543116e844
shah4571 <shahriya4571@gmail.com>
2025-08-06 00:28:37 +0600

    withdraw.py

--- bot/handlers/withdraw.py
+++ bot/handlers/withdraw.py
@@ -1,4 +1,11 @@
-def init(app):
-    @app.on_message(filters.command("withdraw"))
-    async def withdraw(client, message):
-        await message.reply("💸 Withdraw feature coming soon!")
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+def init(app: Client):
+    @app.on_message(filters.command("withdraw") & filters.private)
+    async def withdraw_command(client: Client, message: Message):
+        """
+        💸 Withdraw কমান্ড হ্যান্ডলার।
+        বর্তমানে ফিচারটি সক্রিয় নয়, ভবিষ্যতে যুক্ত হবে।
+        """
+        await message.reply("💸 Withdraw ফিচারটি খুব শীঘ্রই আসছে! অনুগ্রহ করে অপেক্ষা করুন।")
